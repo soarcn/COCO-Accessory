508d0ce332e482c7c6b729998db5732856be7819
soar <soarcn@gmail.com>
2014-05-14 21:26:48 +0800

    animationlistener and view animator enhance

---
+++ views/src/main/java/com/cocosw/accessory/views/AnimationAdapter.java
@@ -0,0 +1,24 @@
+package com.cocosw.accessory.views;
+
+import android.view.animation.Animation;
+
+/**
+ * Project: Accessory
+ * Created by LiaoKai(soarcn) on 2014/5/14.
+ */
+public class AnimationAdapter implements Animation.AnimationListener {
+    @Override
+    public void onAnimationStart(Animation animation) {
+
+    }
+
+    @Override
+    public void onAnimationEnd(Animation animation) {
+
+    }
+
+    @Override
+    public void onAnimationRepeat(Animation animation) {
+
+    }
+}
