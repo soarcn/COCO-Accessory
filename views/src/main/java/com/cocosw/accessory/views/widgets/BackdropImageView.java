b489269fbee933b3e318b3bea513f6b57b383811
soar <soarcn@gmail.com>
2014-12-14 11:17:28 +0800

    new layouts refactory backdropimageview

--- views/src/main/java/com/cocosw/accessory/views/widgets/BackdropImageView.java
+++ views/src/main/java/com/cocosw/accessory/views/widgets/BackdropImageView.java
@@ -2,9 +2,12 @@ package com.cocosw.accessory.views.widgets;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.support.v4.view.ViewCompat;
 import android.util.AttributeSet;
 
+import com.cocosw.accessory.views.Colour;
 import com.cocosw.accessory.views.foreground.ForegroundImageView;
 
 /**
@@ -14,33 +17,68 @@ import com.cocosw.accessory.views.foreground.ForegroundImageView;
  * Created by LiaoKai(soarcn) on 2014/8/29.
  */
 public class BackdropImageView extends ForegroundImageView {
-    public BackdropImageView(Context context) {
-        super(context);
-    }
+
+    private static final int MIN_SCRIM_ALPHA = 20;
+    private static final int MAX_SCRIM_ALPHA = 180;
+    private static final int SCRIM_ALPHA_DIFF = MAX_SCRIM_ALPHA - MIN_SCRIM_ALPHA;
+
+    private float mScrimDarkness;
+    private int mScrimColor = Color.TRANSPARENT;
+    private int mScrollOffset;
+    private int mImageOffset;
+
+    private final Paint mScrimPaint;
+
+    private boolean scrimEnable = false;
 
     public BackdropImageView(Context context, AttributeSet attrs) {
         super(context, attrs);
+        mScrimPaint = new Paint();
     }
 
-    private int mOffset;
+    public void setScrollOffset(int offset) {
+        if (offset != mScrollOffset) {
+            mScrollOffset = offset;
+            mImageOffset = -offset / 2;
+            mScrimDarkness = Math.abs(offset / (float) getHeight());
+            offsetTopAndBottom(offset - getTop());
 
-    public void offsetBackdrop(int offset) {
-        if (offset != mOffset) {
-            mOffset = offset;
+            ViewCompat.postInvalidateOnAnimation(this);
+        }
+    }
+
+    @Override
+    protected void onLayout(boolean changed, int left, int top, int right, int bottom) {
+        super.onLayout(changed, left, top, right, bottom);
+
+        if (mScrollOffset != 0) {
+            offsetTopAndBottom(mScrollOffset - getTop());
+        }
+    }
+
+    public void setScrimColor(int scrimColor) {
+        if (mScrimColor != scrimColor) {
+            mScrimColor = scrimColor;
             ViewCompat.postInvalidateOnAnimation(this);
         }
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
-        if (mOffset != 0) {
+        // Update the scrim paint
+        mScrimPaint.setColor(Colour.setColorAlpha(mScrimColor,
+                MIN_SCRIM_ALPHA + (int) (SCRIM_ALPHA_DIFF * mScrimDarkness)));
+
+        if (mImageOffset != 0) {
             canvas.save();
-            canvas.translate(0f, mOffset);
-            canvas.clipRect(0f, 0f, canvas.getWidth(), canvas.getHeight() + mOffset);
+            canvas.translate(0f, mImageOffset);
+            canvas.clipRect(0f, 0f, canvas.getWidth(), canvas.getHeight() + mImageOffset + 1);
             super.onDraw(canvas);
+            canvas.drawRect(0, 0, canvas.getWidth(), canvas.getHeight(), mScrimPaint);
             canvas.restore();
         } else {
             super.onDraw(canvas);
+            canvas.drawRect(0, 0, canvas.getWidth(), canvas.getHeight(), mScrimPaint);
         }
     }
 }
